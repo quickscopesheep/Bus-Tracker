2a9cf405ab50ba51363395d565dbb50758bb0653
Noah Whewall <nwhewall7@gmail.com>
2025-11-18 14:06:55 +0000

    more restructuring

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ import json
 
 import api
 
-app = Flask(__name__)
+app = Flask(import_name='backend')
 
-@app.route("/")
+@app.route('/search')
+@app.route('/')
 def search_route():
     return app.send_static_file('search.html')
 
-@app.route("/results")
+@app.route('/results')
 def results_route():
     return app.send_static_file('results.html')
 
-@app.route("/timetable")
+@app.route('/timetable')
 def timetable_route():
     return app.send_static_file('timetable.html')
 
-@app.route("/api")
+@app.route('/api')
 def api_route():
     search_body = request.args.get('body', '')
     response_struct = api.build_search_result(search_body)
--- run.bat
+++
@@ -1,1 +0,0 @@
-python -m flask --app backend/app run 
